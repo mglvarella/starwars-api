130bf55bd6cbc409963fe3eecea6c9061e049eea
Miguel Varella <mig-varella1@hotmail.com>
2026-02-05 21:10:36 -0300

    getting /docs out of the api key necessity

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ from app.utils.security import get_api_key
 
 app = FastAPI(
     title="Star Wars API - PowerOfData",
-    dependencies=[Depends(get_api_key)],
     redirect_slashes=True
 )
 
-app.include_router(films.router, prefix="/films", tags=["films"])
-app.include_router(species.router, prefix="/species", tags=["species"])
-app.include_router(people.router, prefix="/people", tags=["people"])
-app.include_router(planets.router, prefix="/planets", tags=["planets"])
-app.include_router(starships.router, prefix="/starships", tags=["starships"])
-app.include_router(vehicles.router, prefix="/vehicles", tags=["vehicles"])
+api_key_dependency = [Depends(get_api_key)]
+
+app.include_router(films.router, prefix="/films", tags=["films"], dependencies=api_key_dependency)
+app.include_router(species.router, prefix="/species", tags=["species"], dependencies=api_key_dependency)
+app.include_router(people.router, prefix="/people", tags=["people"], dependencies=api_key_dependency)
+app.include_router(planets.router, prefix="/planets", tags=["planets"], dependencies=api_key_dependency)
+app.include_router(starships.router, prefix="/starships", tags=["starships"], dependencies=api_key_dependency)
+app.include_router(vehicles.router, prefix="/vehicles", tags=["vehicles"], dependencies=api_key_dependency)
